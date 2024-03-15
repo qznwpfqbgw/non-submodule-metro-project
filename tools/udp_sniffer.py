b7bb3a438fd049919683854131e5a7bd0285cae2
fourcolor <james1qaz2wsx12qw@gmail.com>
2024-03-15 15:44:31 +0800

    Add udp sniffer tool

---
+++ tools/udp_sniffer.py
@@ -0,0 +1,163 @@
+#!/usr/bin/python
+
+from bcc import BPF
+import time
+import sys
+import argparse
+
+PACKET_SIZE = 250
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i", "--device",
+    help="net_interface")
+parser.add_argument("-L", "--localport",
+    help="comma-separated list of local ports to trace.")
+parser.add_argument("-D", "--remoteport",
+    help="comma-separated list of remote ports to trace.")
+
+
+bpf_text = """
+
+#include <uapi/linux/ptrace.h>
+#include <net/sock.h>
+#include <bcc/proto.h>
+#include <linux/bpf.h>
+
+#define IP_TCP 6
+#define IP_UDP 17
+#define IP_ICMP 1
+#define ETH_HLEN 14
+
+BPF_PERF_OUTPUT(skb_events);
+ 
+struct payload {
+    int len;
+};
+
+int packet_monitor(struct __sk_buff *skb) {
+    u8 *cursor = 0;
+    u32 saddr, daddr;
+    u32 sport, dport;
+    u32 udp_header_length = 0;
+    u32 ip_header_length = 0;
+    u32 payload_offset = 0;
+    u32 payload_length = 0;
+    
+    struct ethernet_t *ethernet = cursor_advance(cursor, sizeof(*ethernet));
+    struct ip_t *ip = cursor_advance(cursor, sizeof(*ip));
+
+    if (ip->ver != 4)
+        goto KEEP;
+
+    ip_header_length = ip->hlen << 2;  // SHL 2 -> *4 multiply
+
+    /* check ip header length against minimum */
+    if (ip_header_length < sizeof(*ip))
+        goto KEEP;
+        
+    if (ip->nextp == IP_TCP) 
+        goto KEEP;
+    if (ip->nextp == IP_ICMP) 
+        goto KEEP;
+        
+    if (ip -> nextp == IP_UDP){
+        struct udp_t *udp = cursor_advance(cursor, sizeof(*udp));
+        payload_offset = ETH_HLEN + ip_header_length + 8;
+        payload_length = ip->tlen - ip_header_length - 8;
+        dport = udp->dport;
+        sport = udp->sport;
+        saddr = ip -> src;
+        daddr = ip -> dst;
+        int len = payload_length;
+        FILTER_DPORT
+        FILTER_SPORT
+        skb_events.perf_submit_skb(skb, skb->len, &len, sizeof(len));
+    }
+    
+/* keep the packet and send it to userspace returning -1 */
+KEEP:
+    return -1;
+
+/* drop the packet returning 0 */
+DROP:
+    return 0;
+}
+
+"""
+
+from ctypes import *
+import ctypes as ct
+import sys
+import socket
+import os
+import struct
+import ipaddress
+import ctypes
+from datetime import datetime
+from struct import unpack
+
+if args.remoteport:
+    dports = [int(dport) for dport in args.remoteport.split(',')]
+    dports_if = ' && '.join(['dport != %d' % dport for dport in dports])
+    bpf_text = bpf_text.replace('FILTER_DPORT',
+        'if (%s) { goto KEEP; }' % dports_if)
+if args.localport:
+    lports = [int(lport) for lport in args.localport.split(',')]
+    lports_if = ' && '.join(['lport != %d' % lport for lport in lports])
+    bpf_text = bpf_text.replace('FILTER_SPORT',
+        'if (%s) { goto KEEP; }' % lports_if)
+
+OUTPUT_INTERVAL = 1
+
+bpf = BPF(text=bpf_text)
+
+function_skb_matching = bpf.load_func("packet_monitor", BPF.SOCKET_FILTER)
+
+BPF.attach_raw_socket(function_skb_matching, parser.net_interface)
+
+def payload_info(cpu, data, size):
+    class SkbEvent(ct.Structure):
+        _fields_ = [
+            ("len", ct.c_uint32),
+            ("raw", ct.c_ubyte * (size - ct.sizeof(ct.c_uint32)))
+        ]
+    try:
+        sk = ct.cast(data, ct.POINTER(SkbEvent)).contents
+        ip_packet = bytes(sk.raw[14:])
+        (length, _, _, _, _, proto, _, saddr, daddr) = unpack('!BBHLBBHLL', ip_packet[:20])
+        len_iph = length & 15
+        # Length is written in 32-bit words, convert it to bytes:
+        len_iph = len_iph * 4
+        saddr = ".".join(map(str, [saddr >> 24 & 0xff, saddr >> 16 & 0xff, saddr >> 8 & 0xff, saddr & 0xff]))
+        daddr = ".".join(map(str, [daddr >> 24 & 0xff, daddr >> 16 & 0xff, daddr >> 8 & 0xff, daddr & 0xff]))
+        udp_packet = ip_packet[len_iph:]
+        sport = int.from_bytes(udp_packet[0:2],"big")
+        dport = int.from_bytes(udp_packet[2:4],"big")
+        payload = udp_packet[8:]
+        packet_nums = int(sk.len/PACKET_SIZE)
+        ts = datetime.now()
+        for i in range(packet_nums):
+            """
+            tag         epoch       microseconds    seq
+            8 bytes     4 bytes     4bytes          4 bytes
+            -------------------------------------------------------
+            |    0-8    |    8-12   |    12-16      |    16-20    |
+            -------------------------------------------------------
+            """
+            custom_header = payload[PACKET_SIZE*i : PACKET_SIZE*i + 40]
+            epoch = int.from_bytes(custom_header[8:12],"big")
+            microseconds = int.from_bytes(custom_header[12:16],"big")
+            seq = int.from_bytes(custom_header[16:20],"big")
+            print(f"{ts},{saddr},{daddr},{sport},{dport},{seq},{epoch},{microseconds},")
+    except ValueError:
+        return "Invalid input"
+
+try:
+    bpf["skb_events"].open_perf_buffer(payload_info)
+    print("timestamp,saddr,daddr,sport,dport,sequence,epoch,microseconds")
+    while True :
+        bpf.perf_buffer_poll()
+        
+except KeyboardInterrupt:
+    sys.stdout.close()
+    pass
