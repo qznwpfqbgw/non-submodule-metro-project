5946bd24f005e10cd59213d6cfe18d41b1e4441d
fourcolor <james1qaz2wsx12qw@gmail.com>
2024-05-06 17:28:28 +0800

    Add git_submodule_restore

---
+++ git_submodule_restore.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+cd tcp-upload/ && git restore . && cd ..
+cd Dynamic-Band-Locking/ && git restore . && cd ..
+cd quic_upload/ && git restore . && cd ..
+cd udp/ && git restore . && cd ..
