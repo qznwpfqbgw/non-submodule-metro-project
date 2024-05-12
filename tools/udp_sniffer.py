a1a788bb101a68aa4fed6bb0be27cccb777df704
Shi-Sheng Yang <fourcolor4c@gmail.com>
2024-05-13 02:31:39 +0800

    Fix udp_sniffer issue

--- tools/udp_sniffer.py
+++ tools/udp_sniffer.py
@@ -4,6 +4,13 @@ from bcc import BPF
 import time
 import sys
 import argparse
+import socket
+import os
+from datetime import datetime
+from struct import unpack
+import fcntl
+import struct
+import netifaces
 
 PACKET_SIZE = 250
 
@@ -33,7 +40,7 @@ struct perf_data {
     int dir;
 };
 
-int packet_monitor(struct __sk_buff *skb) {
+int packet_filter(struct __sk_buff *skb) {
     u8 *cursor = 0;
     u32 saddr, daddr;
     u32 sport, dport;
@@ -46,82 +53,87 @@ int packet_monitor(struct __sk_buff *skb) {
     struct ip_t *ip = cursor_advance(cursor, sizeof(*ip));
 
     if (ip->ver != 4)
-        goto KEEP;
+        goto DROP;
 
     ip_header_length = ip->hlen << 2;  // SHL 2 -> *4 multiply
 
     /* check ip header length against minimum */
     if (ip_header_length < sizeof(*ip))
-        goto KEEP;
+        goto DROP;
         
     if (ip->nextp == IP_TCP) 
-        goto KEEP;
+        goto DROP;
     if (ip->nextp == IP_ICMP) 
-        goto KEEP;
+        goto DROP;
         
     if (ip -> nextp == IP_UDP){
         struct udp_t *udp = cursor_advance(cursor, sizeof(*udp));
-        payload_offset = ETH_HLEN + ip_header_length + 8;
-        payload_length = ip->tlen - ip_header_length - 8;
         dport = udp->dport;
         sport = udp->sport;
         saddr = ip -> src;
         daddr = ip -> dst;
-        int len = payload_length;
         FILTER_PORT
-        struct perf_data data = {.len = len, .dir = skb->ingress_ifindex};
-        skb_events.perf_submit_skb(skb, skb->len, &data, sizeof(data));
     }
     
-/* keep the packet and send it to userspace returning -1 */
-KEEP:
-    return -1;
+
 
 /* drop the packet returning 0 */
 DROP:
     return 0;
+
+/* keep the packet and send it to userspace returning -1 */
+KEEP:
+    return -1;
+
 }
 
 """
 
-from ctypes import *
-import ctypes as ct
-import sys
-import socket
-import os
-import struct
-import ipaddress
-import ctypes
-from datetime import datetime
-from struct import unpack
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s',  bytes(ifname[:15], 'utf-8'))
+    )[20:24])
+
+def get_arp_hwtype(interface):
+    try:
+        addrs = netifaces.ifaddresses(interface)
+        arp_hwtype = addrs[netifaces.AF_LINK][0]['addr']
+        return arp_hwtype
+    except KeyError:
+        return None
 
 out = sys.stdout
 if args.file:
     out = open(args.file,"w+")
 if args.ports:
     dports = [int(port) for port in args.ports.split(',')]
-    dports_if = ' && '.join([f'dport != {port} && sport != {port}' for port in dports])
+    dports_if = ' || '.join([f'dport == {port} || sport == {port}' for port in dports])
     bpf_text = bpf_text.replace('FILTER_PORT',
         'if (%s) { goto KEEP; }' % dports_if)
 else:
-    bpf_text = bpf_text.replace('FILTER_PORT',"")
+    bpf_text = bpf_text.replace('FILTER_PORT',"goto KEEP;")
 
 bpf = BPF(text=bpf_text)
 
-function_skb_matching = bpf.load_func("packet_monitor", BPF.SOCKET_FILTER)
+function_skb_matching = bpf.load_func("packet_filter", BPF.SOCKET_FILTER)
 
 BPF.attach_raw_socket(function_skb_matching, args.device)
+host_if_ip = get_ip_address(args.device)
+host_has_layer2 = get_arp_hwtype(args.device) != None 
+socket_fd = function_skb_matching.sock
+fl = fcntl.fcntl(socket_fd, fcntl.F_GETFL)
+fcntl.fcntl(socket_fd, fcntl.F_SETFL, fl & (~os.O_NONBLOCK))
 
-def payload_info(cpu, data, size):
-    class SkbEvent(ct.Structure):
-        _fields_ = [
-            ("len", ct.c_uint32),
-            ("dir", ct.c_uint32),
-            ("raw", ct.c_ubyte * (size - 2*ct.sizeof(ct.c_uint32))),
-        ]
-    try:
-        sk = ct.cast(data, ct.POINTER(SkbEvent)).contents
-        ip_packet = bytes(sk.raw[14:])
+try:
+    print("timestamp,saddr,daddr,sport,dport,sequence,epoch,microseconds,direction",file=out)
+    while True:
+        if host_has_layer2:
+            ip_packet = bytes(os.read(socket_fd, 4096)[14:])
+        else:
+            ip_packet = bytes(os.read(socket_fd, 4096))
         (length, _, _, _, _, proto, _, saddr, daddr) = unpack('!BBHLBBHLL', ip_packet[:20])
         len_iph = length & 15
         # Length is written in 32-bit words, convert it to bytes:
@@ -131,8 +143,9 @@ def payload_info(cpu, data, size):
         udp_packet = ip_packet[len_iph:]
         sport = int.from_bytes(udp_packet[0:2],"big")
         dport = int.from_bytes(udp_packet[2:4],"big")
+        packet_len = int.from_bytes(udp_packet[4:6],"big")
         payload = udp_packet[8:]
-        packet_nums = int(sk.len/PACKET_SIZE)
+        packet_nums = int(packet_len/PACKET_SIZE)
         ts = datetime.now()
         for i in range(packet_nums):
             """
@@ -146,16 +159,8 @@ def payload_info(cpu, data, size):
             epoch = int.from_bytes(custom_header[8:12],"big")
             microseconds = int.from_bytes(custom_header[12:16],"big")
             seq = int.from_bytes(custom_header[16:20],"big")
-            dir = sk.dir
-            print(f"{ts},{saddr},{daddr},{sport},{dport},{seq},{epoch},{microseconds},{dir},",file=out,flush=True)
-    except ValueError:
-        return "Invalid input"
-try:
-    bpf["skb_events"].open_perf_buffer(payload_info)
-    print("timestamp,saddr,daddr,sport,dport,sequence,epoch,microseconds,direction",file=out,flush=True)
-    while True :
-        bpf.perf_buffer_poll()
-        
+            dir = 0 if host_if_ip == saddr else 1
+            print(f"{ts},{saddr},{daddr},{sport},{dport},{seq},{epoch},{microseconds},{dir}",file=out,flush=True)
 except KeyboardInterrupt:
     sys.stdout.close()
     pass
